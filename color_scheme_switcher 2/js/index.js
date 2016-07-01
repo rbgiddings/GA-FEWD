1971709d23ec7635e408e61f35400c35827887fd
rbgiddings <Greg@RebeccaMac.local>
2016-06-30 21:26:24 -0500

    in-class work 6/30/16

---
+++ color_scheme_switcher 2/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	$('#grayButton').click(switchGray);
+	$('#whiteButton').click(switchWhite);
+
+});
+
+function switchGray() {
+  $('body').css('background-color','gray');
+  $('body').css('color','white');
+}
+
+function switchWhite() {
+  $('body').css('background-color','white');
+  $('body').css('color','black');
+}
